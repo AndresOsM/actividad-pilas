d359d5df1f3d349f8778dfdb8baa0b5edd182ea8
paolabotero-creador <paola.botero@est.iudigital.edu.co>
2025-09-20 22:14:57 -0500

    Se crean comentarios en la líneas de código hasta el caso 3

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,15 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Stack<String> myStackOne = new Stack<>();
-        Stack<String> myStackTwo = new Stack<>();
+        //Inicialización de pilas y variables
+        Stack<String> myStackOne = new Stack<>(); //Pila inicial
+        Stack<String> myStackTwo = new Stack<>(); //Pila secundaria
         int op;
         String text;
 
+        // Implementación de do while
         do {
+            // Creación de menú consola
             System.out.println("Bienvenido !!\n");
 
             System.out.println("Escoja la accion que desea realizar (1-5)\n");
@@ -23,34 +26,36 @@ public class Main {
             op = sc.nextInt();
             sc.nextLine();
 
+            // Implementación para entrar a cada caso del menú consola
             switch (op) {
-                case 1:
+                case 1: //Ingresar por consola el texto
                     System.out.println("Escribir texto");
                     text = sc.nextLine();
 
-                    myStackOne.push(text);
-                    System.out.println(myStackOne);
+                    myStackOne.push(text); //Agregar el texto ingresado a la pila 1
+                    System.out.println(myStackOne); //Imprime la pila 1
                     break;
 
-                case 2:
-                    if (!myStackOne.isEmpty()){
-                        myStackTwo.push(myStackOne.pop());
+                case 2: //Borra el último texto de la pila 1
+                    if (!myStackOne.isEmpty()){ //Valida sí la pila 1 tiene datos
+                        myStackTwo.push(myStackOne.pop()); //.pop elimina pila 1 y .push añade a pila 2
                         System.out.println("Texto borrado");
                     } else{
-                        System.out.println("No se encontro texto para borrar");
+                        System.out.println("No se encontro texto para borrar"); //Sí la pila vacía muestra mensaje
                     }
-                    System.out.println(myStackOne);
-                    System.out.println(myStackTwo);
+                    System.out.println(myStackOne); //Imprime pila 1
+                    System.out.println(myStackTwo); //Imprime pila 2
                     break;
 
-                case 3:
-                    if (!myStackTwo.isEmpty()){
-                        myStackOne.push(myStackTwo.pop());
+                case 3: //Rehace el texto de la pila 1
+                    if (!myStackTwo.isEmpty()){ //Valida sí la pila 2 tiene datos
+                        myStackOne.push(myStackTwo.pop()); //.pop elimina la pila 2 y .push inserta la pila 1
                         System.out.println("Texto reconstruido");
                     } else{
-                        System.out.println("No se encontro texto para rehacer");
+                        System.out.println("No se encontro texto para rehacer"); //Sí la pila vacía muestra mensaje
                     }
-                    System.out.println(myStackOne);
+                    System.out.println(myStackOne); //Imprime pila 1
+                    System.out.println(myStackTwo); //Imprime pila 2
                     break;
 
                 case 4:
